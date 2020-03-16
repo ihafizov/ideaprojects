d4dc54c83efaa02ae9afeda3f7c6b8b940bb3df9
ihafizov <ii.hafizov@yandex.ru>
2020-03-16 22:43:28 +0500

    modding on mac

--- test1/src/Test1.java
+++ test1/src/Test1.java
@@ -5,5 +5,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("test1");
         System.out.println("test2");
+        System.out.println("i'm still working on this module");
     }
 }
